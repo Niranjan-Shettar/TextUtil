defafee7c6b809f6cd959b1ba1c2e3160fc58b63
Niranjan-Shettar <shettarniranjan@gmail.com>
2022-11-10 12:20:18 +0530

    Updates

---
+++ precache-manifest.be1feaa17bfefd38b253f0e72cb5a675.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c8fa1ae93e85bc087780",
+    "url": "/TextUtil/static/css/main.70ff2180.chunk.css"
+  },
+  {
+    "revision": "c8fa1ae93e85bc087780",
+    "url": "/TextUtil/static/js/main.c8fa1ae9.chunk.js"
+  },
+  {
+    "revision": "cc360fb6938c4a946641",
+    "url": "/TextUtil/static/js/1.cc360fb6.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/TextUtil/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "fa5c1e91450aa65b75cd",
+    "url": "/TextUtil/static/js/runtime~main.fa5c1e91.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/TextUtil/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "483f7ee826665f961dc08e89dacd9725",
+    "url": "/TextUtil/index.html"
+  }
+];
